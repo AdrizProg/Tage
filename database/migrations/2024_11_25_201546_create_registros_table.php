f5856b5fca99af549a7613c182d06bfe519fac7c
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2024-11-25 20:18:55 +0000

    Subiendo la ultima migracion de la tabla registro

---
+++ database/migrations/2024_11_25_201546_create_registros_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('registros', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('iniCliclo');
+            $table->unsignedBigInteger('antesde');
+            $table->foreign('antesde')->references('id')->on('antes')->onDelete('cascade');
+            $table->unsignedBigInteger('durante');
+            $table->foreign('durante')->references('id')->on('durantes')->onDelete('cascade');
+            $table->unsignedBigInteger('despuesde');
+            $table->foreign('despuesde')->references('id')->on('despues')->onDelete('cascade');
+            $table->unsignedBigInteger('compostera');
+            $table->foreign('compostera')->references('id')->on('composteras')->onDelete('cascade');
+            $table->unsignedBigInteger('usuario');
+            $table->foreign('usuario')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('registros');
+    }
+};
