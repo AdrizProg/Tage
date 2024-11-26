dd49120ddbeaf8a0aadce0de43a9b3d5e302b0f7
Illian-Santiago <illianclase12345678@gmail.com>
2024-11-26 17:29:23 +0000

    Sin imagen

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,9 +21,6 @@
 
 <body class="flex justify-center items-center min-h-screen">
     <header class="grid grid-cols-2 items-center gap-2 py-10 lg:grid-cols-3">
-        <div class="flex lg:justify-center lg:col-start-2">
-            <img src="{{Vite::asset('/resources/images/icono.png')}}" alt="">
-        </div>
         @if (Route::has('login'))
         <nav class="-mx-3 flex flex-1 justify-end">
             @auth
