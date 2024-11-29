ef0a1cadf619dccefaa04b5cd795767947a53653
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2024-11-29 17:50:50 +0000

    Subiendo una prueba para el yml

--- resources/js/dashboard.js
+++ resources/js/dashboard.js
@@ -5,7 +5,7 @@ export default function dashboardView() {
       <div class="bg-white shadow-md rounded-lg p-6 max-w-sm">
         <h2 class="text-2xl font-bold text-gray-800 mb-4">Hola, Mundo!</h2>
         <p class="text-gray-600 mb-4">
-          La madre del cordero mamita
+          La madre del cordero mamita, otro mas
         </p>
         <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Click Me
