797ccc02b2ff6f9428a0a30c1a39b6ab8496eeb3
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2024-12-03 18:39:09 +0000

    Js, intento dashboar, salio el registro
    El fetch para consultar la api y ver los registros esta hecho, falta funcion pero se muestran los datos de forma correcta, mas adelante desaparecera.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,14 +5,13 @@
                 {{ __('Nueva compostera') }}
             </x-primary-button>
         </a>
-
-        @if (session('token'))
-        <p>Token: {{ session('token') }}</p>
-        @endif
-        <script>
-            sessionStorage.setItem('apiToken', @json(session('token')))
-        </script>
     </x-slot>
 
     <div id="dash"></div>
+    <!-- @if (session('token'))
+        <p>Token: {{ session('token') }}</p>
+        @endif -->
+        <script>
+            sessionStorage.setItem('apiToken', @json(session('token')))
+        </script>
 </x-app-layout>
