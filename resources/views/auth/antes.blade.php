efa161f88b8f20573ff1436d079d6fe8fddb3679
Illian-Santiago <illianclase12345678@gmail.com>
2024-12-05 12:08:28 +0000

    quitado ootros

--- resources/views/auth/antes.blade.php
+++ resources/views/auth/antes.blade.php
@@ -64,7 +64,6 @@
                     <option>Podrido</option>
                     <option>Amoniaco</option>
                     <option>Sin olor</option>
-                    <option>Otro</option>
                 </select>
                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -85,7 +84,6 @@
                     <option>Moscas</option>
                     <option>Larbas</option>
                     <option>Ratones</option>
-                    <option>Otro</option>
                 </select>
                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -106,7 +104,6 @@
                     <option>Muy mojado</option>
                     <option>Bien</option>
                     <option>Seco</option>
-                    <option>Otro</option>
                 </select>
                 <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
                     <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
