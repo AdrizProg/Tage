f891fca6525bfa44002100b9121386415ca3bcee
AdrizProg <157852821+AdrizProg@users.noreply.github.com>
2024-12-10 21:56:07 +0000

    Implementacion de algunas funcionalidades pequeñas de botones del formulario y alert

--- resources/js/rutas.js
+++ resources/js/rutas.js
@@ -30,11 +30,17 @@ export default function rutasUrl() {
         boton.addEventListener('click', () => {
             location.replace('/registro#antes' + id);
         });
-        
 
-    }else if (hash.includes('/registro#durante')) {
+
+    } else if (hash.includes('/registro#durante')) {
 
         mostrarRegistros(2);
+
+        const idComp = window.location.href.slice(-1);
+        const btnAnterior = document.getElementById('btnAnterior');
+        btnAnterior.addEventListener('click', async () => {
+            location.replace('/registro#antes' + idComp);
+        });
 
         const boton = document.getElementById('boton');
         boton.addEventListener('click', async () => {
@@ -72,7 +78,38 @@ export default function rutasUrl() {
         const boton = document.getElementById('boton');
         boton.addEventListener('click', async () => {
 
-            uploadAntes();
+            const idComp = window.location.href.slice(-1);
+
+            const alertHTML = `
+                <div id="alert-message" class="fixed top-5 right-5 bg-red-500 text-white px-4 py-2 rounded shadow-md text-sm opacity-0 transition-opacity duration-300">
+                    El registro ya ha sido introducido, acabe todos los formularios o cancele el registro.
+                </div>
+                            `;
+
+            // Insertar el mensaje de alerta en el cuerpo del documento
+            document.body.insertAdjacentHTML('beforeend', alertHTML);
+
+            if (localStorage.getItem('antes' + idComp)) {
+                const alertElement = document.getElementById('alert-message');
+                if (alertElement) {
+                    alertElement.classList.remove('opacity-0'); // Hacer visible el mensaje
+                    alertElement.classList.add('opacity-100');
+
+                    // Ocultar el mensaje después de 3 segundos
+                    setTimeout(() => {
+                        alertElement.classList.remove('opacity-100');
+                        alertElement.classList.add('opacity-0');
+
+                        // Eliminar el elemento del DOM después de la animación de salida
+                        setTimeout(() => alertElement.remove(), 1000);
+                    }, 3000);
+                }
+
+                location.replace('/registro#durante' + idComp);
+            } else {
+                uploadAntes();
+            }
+
 
         });
 
