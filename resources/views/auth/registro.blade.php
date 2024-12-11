b2c533ff8be6fdc8f618a70ab20dbaa040bc4672
Illian-Santiago <illianclase12345678@gmail.com>
2024-12-11 15:12:02 +0000

    Crear registro es responsive

--- resources/views/auth/registro.blade.php
+++ resources/views/auth/registro.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
-    <div id="formularios" class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md" style="margin-left: 27%;">
-        <div id="formularios"> </div>
-
+    <div class="flex justify-center">
+        <div id="formularios" class="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
+        </div>
     </div>
 </x-app-layout>
